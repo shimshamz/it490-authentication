f55d54d98f00b1ddff53c1e74e7524b1551b96f7
root <sm2292@njit.edu>
2019-02-23 16:50:10 -0500

    Troubleshooting

---
+++ rabbitRegister.php
@@ -0,0 +1,38 @@
+#usr/bin/php
+<?php
+require_once('/home/shameel/it490-rabbit/rabbitmqphp_example/path.inc');
+require_once('/home/shameel/it490-rabbit/rabbitmqphp_example/get_host_info.inc');
+require_once('/home/shameel/it490-rabbit/rabbitmqphp_example/rabbitMQLib.inc');
+
+$client = new rabbitMQClient("/home/shameel/it490-rabbit/rabbitmqphp_example/testRabbitMQ.ini","testServer");
+if (isset($argv[1]))
+{
+  $msg = $argv[1];
+}
+else
+{
+  $msg = "test message";
+}
+
+$request = array();
+$request['type'] = "register";
+
+#$request['fname'] = "test";
+#$request['lname'] = "test";
+#$request['email'] = "user3@gmail.com";
+#$request['password'] = "1234";
+
+$request['fname'] = $_POST['fname'];
+$request['lname'] = $_POST['lname'];
+$request['email'] = $_POST['email'];
+$request['password'] = $_POST['pass'];
+$request['message'] = $msg;
+$response = $client->send_request($request);
+//$response = $client->publish($request);
+
+echo "client received response: ".PHP_EOL;
+print_r($response);
+echo "\n\n";
+
+echo $argv[0]." END".PHP_EOL;
+?>
